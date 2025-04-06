446d8d8176e480dcb3ebdbd2f61fac78a4095618
Justin Chapman <commonmodestudio@gmail.com>
2025-04-06 09:37:22 -0400

    interpreter

--- src/interpreter.zig
+++ src/interpreter.zig
@@ -9,8 +9,24 @@ pub const Interpreter = struct {
             .root = root,
         };
     }
-    /// TODO
-    pub fn interpret() u64 {
-        return 5;
+    pub fn interpret(self: *Interpreter) u64 {
+        return self.visit(self.root);
+    }
+    fn visit(self: *Interpreter, node: *ASTNode) u64 {
+        switch (node.*) {
+            .Number => |number| {
+                return number;
+            },
+            .BinOp => |bin| {
+                const left = self.visit(bin.left);
+                const right = self.visit(bin.right);
+                return switch (bin.op) {
+                    .Add => left + right,
+                    .Sub => left - right,
+                    .Mul => left * right,
+                    .Div => left / right,
+                };
+            },
+        }
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -25,6 +25,8 @@ pub fn main() void {
     var parsr = Parser.new(lexr.tokens.items, allocator);
     const root = parsr.parse();
     defer parsr.deinit(root);
+    var interp = Interpreter.new(root);
+    const result = interp.interpret();
     const stdout = std.io.getStdOut().writer();
     // pretty print ast
     var ws = std.json.writeStream(
@@ -32,7 +34,7 @@ pub fn main() void {
         .{ .whitespace = .indent_2 },
     );
     defer ws.deinit();
-    ws.write(root) catch unreachable;
+    ws.print("{d}\n", .{result}) catch unreachable;
 }
 
 test "lex" {
@@ -71,7 +73,6 @@ test "parse" {
     try std.testing.expectEqual(1, root.BinOp.right.Number);
 }
 
-// TODO
 test "interpret" {
     const source = "(+ (* 2 2) 1)";
     var lex = Lexer.new(source, std.testing.allocator);
@@ -80,6 +81,7 @@ test "interpret" {
     var parsr = Parser.new(lex.tokens.items, std.testing.allocator);
     const root = parsr.parse();
     defer parsr.deinit(root);
-    _ = Interpreter.new(root);
-    try std.testing.expect(true);
+    var interp = Interpreter.new(root);
+    const result = interp.interpret();
+    try std.testing.expectEqual(5, result);
 }
